ffdddbc6e697bd5b824bc60569935397f060ba99
Dahmane Mounir <dahmanemounirbaligh@gmail.com>
2026-04-06 01:42:10 +0100

    finding the object files, adding Cmake, improving the shaders files, source files, LICENCE and git ignore, and fixing multiple bugs to migrate from windows to linux

---
+++ build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/dependencies/glad/glad/glad.c.o"
+  "CMakeFiles/main.dir/dependencies/glad/glad/glad.c.o.d"
+  "CMakeFiles/main.dir/src/3D-Chess.cpp.o"
+  "CMakeFiles/main.dir/src/3D-Chess.cpp.o.d"
+  "CMakeFiles/main.dir/src/stb_image_impl.cpp.o"
+  "CMakeFiles/main.dir/src/stb_image_impl.cpp.o.d"
+  "main"
+  "main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
